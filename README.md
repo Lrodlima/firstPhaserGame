f19a7dcc73351a723b47d8a6f4212bf5dd312170
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2016-09-05 00:51:41 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A little experience coding with Phaser
---
+++ src/states/boot.js
@@ -0,0 +1,45 @@
+class Boot extends Phaser.State {
+
+  constructor() {
+    super();
+  }
+
+  preload() {
+    this.load.image('preloader', 'assets/preloader.gif');
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  }
+
+  create() {
+    this.game.input.maxPointers = 1;
+
+    //setup device scaling
+    if (this.game.device.desktop) {
+      this.game.scale.pageAlignHorizontally = true;
+    } else {
+      this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.game.scale.minWidth =  480;
+      this.game.scale.minHeight = 260;
+      this.game.scale.maxWidth = 640;
+      this.game.scale.maxHeight = 480;
+      this.game.scale.forceOrientation(true);
+      this.game.scale.pageAlignHorizontally = true;
+      this.game.scale.setScreenSize(true);
+    }
+
+    this.initGlobalVariables();
+
+    this.game.state.start('preloader');
+  }
+
+  initGlobalVariables(){
+    this.game.global = {
+
+    };
+  }
+
+}
+
+export default Boot;
